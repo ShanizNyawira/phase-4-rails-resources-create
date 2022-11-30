645abebd0f1d05bdd2a3368b5436c3a78fd96d68
Shaniz Nyawira <roselynshaniz@gmail.com>
2022-11-30 08:48:59 +0300

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -15,5 +15,14 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+# POST /birds
+def create
+  bird = Bird.new(bird_params)
+  if bird.save
+    render json: bird, status: :created, location: bird
+  else
+    render json: bird.errors, status: :unprocessable_entity
+  end
+end
 
 end
